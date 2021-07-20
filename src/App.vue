3bbe08429b0477b8b5a3acb7d53c9cad784cc41d
Casper van Battum <cvanbattum@gmail.com>
2021-07-20 13:09:11 +0200

    Fix and clean up legacy vertical navigation

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,12 @@
     <header>
       <h1>Casper van Battum</h1>
     </header>
-    <div class="page-container vertical-navigation">
-      <!--<nav id="nav">
-        <router-link class="slide-ltr" to="/">Home</router-link>
-        <router-link class="slide-ltr" to="/projects">Projects</router-link>
-        <router-link class="slide-ltr" to="/photos">Photography</router-link>
-        <router-link class="slide-ltr" to="/contact">Contact</router-link>
-      </nav>-->
+    <div class="page-container horizontal-navigation">
       <nav id="nav">
-        <router-link class="slide-ltr" to="/">About</router-link>
-        <router-link class="slide-ltr" to="/projects">Projects</router-link>
+        <router-link to="/">Home</router-link>
+        <router-link to="/projects">Projects</router-link>
+<!--        <router-link to="/photos">Photography</router-link>-->
+<!--        <router-link to="/contact">Contact</router-link>-->
       </nav>
       <transition name="fade" mode="out-in">
         <router-view id="router-view"/>
@@ -55,78 +51,71 @@ header h1 {
 
 .vertical-navigation {
   display: flex;
+
+  nav {
+    flex-basis: 170px;
+    flex-shrink: 0;
+    overflow: hidden;
+    text-align: left;
+    margin-right: 20px;
+    border-right: 1px dotted $text-dark;
+
+    a {
+      font-family: "Adelle", sans-serif;
+      font-weight: 700;
+      font-size: 1.125em;
+      text-decoration: none;
+      display: block;
+      padding: .5em;
+      margin: .25em 10px .25em 0;
+
+      transition: $transition;
+      position: relative;
+
+      // Underline animation stolen from https://codepen.io/mxttsco/pen/KgodYm
+      &:before,
+      &:after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 0;
+        height: 2px;
+        margin: 5px 0 0;
+        transition: $transition;
+        opacity: 0;
+        background-color: $accent-main;
+      }
+
+      &:hover {
+        background: none;
+        color: $accent-secondary;
+
+        &:before,
+        &:after {
+          width: 80%;
+          opacity: 1;
+        }
+      }
+    }
+  }
+
+  // No longer sure what this actually changes, maybe try a different active effect...
+
+  //nav a.router-link-exact-active {
+  //  color: $accent-secondary;
+  //}
+  //
+  //nav a.router-link-exact-active:hover {
+  //  color: $accent-main;
+  //
+  //  &.slide-ltr {
+  //    color: $accent-secondary;
+  //  }
+  //}
 }
 
-//nav {
-//  flex-basis: 170px;
-//  flex-shrink: 0;
-//  overflow: hidden;
-//  text-align: left;
-//  margin-right: 20px;
-//  border-right: 1px dotted $text-dark;
-//}
-//
-//// Underline animation stolen from https://codepen.io/mxttsco/pen/KgodYm
-//nav a {
-//  font-family: "Adelle", sans-serif;
-//  font-weight: 700;
-//  font-size: 1.125em;
-//  text-decoration: none;
-//  display: block;
-//  padding: .5em;
-//  margin: .25em 10px .25em 0;
-//
-//  transition: $transition;
-//  position: relative;
-//
-//  &:before,
-//  &:after {
-//    content: "";
-//    position: absolute;
-//    bottom: 0;
-//    width: 0;
-//    height: 2px;
-//    margin: 5px 0 0;
-//    transition: $transition;
-//    opacity: 0;
-//    background-color: $accent-main;
-//  }
-//
-//  &.slide-ltr {
-//    &:before,
-//    &:after {
-//      left: 0;
-//    }
-//  }
-//
-//  &:hover {
-//    background: none;
-//  }
-//}
-//
-//.slide-ltr:hover {;
-//  color: $accent-secondary;
-//
-//  &:before,
-//  &:after {
-//    width: 80%;
-//    opacity: 1;
-//  }
-//}
-//
-//nav a.router-link-exact-active {
-//  color: $accent-secondary;
-//}
-//
-//nav a.router-link-exact-active:hover {
-//  color: $accent-main;
-//
-//  &.slide-ltr {
-//    color: $accent-secondary;
-//  }
-//}
-
-$duration: .2s;
+$duration: 100ms;
 $distance: 8px;
 $easeOutBack: cubic-bezier(0.175, 0.885, 0.320, 1.275);
 
@@ -179,7 +168,7 @@ $easeOutBack: cubic-bezier(0.175, 0.885, 0.320, 1.275);
       }
 
       &:hover,
-      &.router-link-exact-active{
+      &.router-link-exact-active {
         &:before,
         &:after {
           opacity: 1;
